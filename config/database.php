7bdec02e7e120102f24df3b880d6bba096ecf3fd
YukiYamamotty0713 <yuuki330110713@gmail.com>
2025-02-19 13:55:53 +0000

    コンフィグを生成

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            'url' => env('JAWSDB_URL'),  // JAWSDB_URL を使用して接続設定
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
@@ -61,6 +61,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
         
         /**
          * テスト用のmysqlデータベース
